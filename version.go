8c3181bdcbf7d667401d8f4e3d18ccf30c61a7d3
kemokemo <t2wonderland@gmail.com>
2020-12-22 08:33:04 +0900

    chore: improve version info.

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version is semantic version of this app.
-const Version string = "0.1.0"
+var Version = "unset"
 
 // Revision is the revision of this app's binary.
 var Revision = "unset"
